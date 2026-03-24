ffbe636c5643b1451edb1fa1e3ddd6d0060d81bf
ahmadaliyevoyatillo <oyatilloahmadaliyev10@gmail.com>
2026-03-24 15:46:37 +0500

    haha

--- models/index.js
+++ models/index.js
@@ -1,21 +1,21 @@
 const { Sequelize } = require("sequelize")
 
+const isRender = process.env.DATABASE_URL?.includes("render.com")
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    protocol: "postgres",
-    logging: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
+const sequelize = new Sequelize(process.env.DATABASE_URL,{
+ dialect:"postgres",
+ protocol:"postgres",
+ logging:false,
+
+ dialectOptions: isRender ? {
+  ssl:{
+   require:true,
+   rejectUnauthorized:false
+  }
+ } : {}
 })
-const db = {}
-db.sequelize = sequelize
-db.Sequelize = Sequelize
-db.User = require("./user")(sequelize, Sequelize)
-db.Lend = require("./lend")(sequelize, Sequelize)
 
-module.exports = db
+module.exports = {
+ sequelize,
+ Sequelize
+}
